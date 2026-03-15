1bbed7245fc648110e5318ee02f1a49f5ba6a74b
Mohith s <smohithonline@gmail.com>
2026-03-15 23:22:13 +0530

    updated nostra

---
+++ index.js
@@ -0,0 +1,9 @@
+// Simple alert when Shop Now button is clicked
+
+const button = document.querySelector("button")
+
+button.addEventListener("click",function(){
+
+    alert("Welcome to Nostra Store!")
+
+})
